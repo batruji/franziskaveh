e1fe47da0ae9e32eacd89629afb921ba7790b420
batruji <batruji@gmail.com>
2017-06-29 15:44:28 +0300

    Updates from website - plugins and WP

--- wp-content/plugins/types/library/toolset/toolset-common/toolset-forms/classes/class.eforms.php
+++ wp-content/plugins/types/library/toolset/toolset-common/toolset-forms/classes/class.eforms.php
@@ -711,7 +711,7 @@ class Enlimbo_Forms {
         $element['_render']['element'] .= sprintf(' data-wpt-type="%s"', __FUNCTION__);
         $element['_render']['element'] .= $this->_getDataWptId($element);
 
-        /**
+        /*
          * Add html attribute value=""
          *
          * We have key #value and we also have #default-value key
@@ -725,9 +725,16 @@ class Enlimbo_Forms {
          *
          * To get things right we flip the usage of Types fields to handle it the same way as CRED does
          *
+         * The fact that a field comes from Types is determined from its name (sic!). This is very fragile.
+         *
          * START Todo: Types should deliver the correct values instead of flipping it here
          */
-        if( strpos( $element['#name'], 'wpcf[' ) === 0 ) {
+        $is_types_field = (
+	        ( strpos( $element['#name'], 'wpcf[' ) === 0 )
+	        || ( strpos( $element['#name'], 'wpcf_post_relationship[' ) === 0 )
+        );
+
+        if( $is_types_field ) {
             $tmp_value = $element['#value'];
 
             $element['#value'] = array_key_exists( '#default_value', $element )
@@ -1165,7 +1172,7 @@ class Enlimbo_Forms {
             if ($element['#type'] == 'file') {
                 return $_FILES[$name]['tmp_name'];
             }
-            return isset($_REQUEST[$name]) ? $_REQUEST[$name] : in_array($element['#type'], array('textfield', 'textarea')) ? '' : 0;
+            return isset($_REQUEST[$name]) ? sanitize_text_field( $_REQUEST[$name] ) : in_array($element['#type'], array('textfield', 'textarea')) ? '' : 0;
         }
 
         $parts = explode('[', $name);
